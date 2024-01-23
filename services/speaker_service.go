579c28a4c5fe5dd34822e42e4e4e942e839a9f8a
Marc Catrisse <marc@catrisse.net>
2024-01-23 23:37:55 +0100

    add mutex in playNotification

--- services/speaker_service.go
+++ services/speaker_service.go
@@ -3,12 +3,16 @@ package services
 import (
 	"log"
 	"os/exec"
+	"sync"
 )
 
 func PlayNotification(path string) error {
+	var m sync.Mutex
+	m.Lock() //Add Mutex to avoid multiple plays at the same time
 	cmd := exec.Command("mplayer", path)
 	log.Printf("Running Command -> mplayer %s\n", path)
 	out, err := cmd.Output()
+	m.Unlock()
 	if err != nil {
 		out = []byte(err.Error())
 	}
